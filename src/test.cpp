7eba0ac5c700abdbd9fdcf72dd2a8794f50d75b8
root <root@Ubuntu22.04.myguest.virtualbox.org>
2024-05-14 18:47:53 +0800

    测试发送给网卡的数据,接收修改并发送回去。需要宿主机发送广播数据给虚拟机，测试成功

--- src/test.cpp
+++ src/test.cpp
@@ -1,3 +1,4 @@
+#include "pcap.h"
 #include "sys_plat.h"
 #include <pcap/pcap.h>
 #include <string.h>
@@ -5,28 +6,36 @@
 int main()
 {
     PcapNICDriver driver("192.168.56.101");
-
-    // 00:15:5d:fd:4b:3e
     uint8_t mac_arr[] = { 0x80, 0x00, 0x27, 0xcf, 0x16, 0xfb };
-
     pcap_t* pcap = driver.DeviceOpen(mac_arr);
+    u_char buf[1024];
+    memset(buf, 1, sizeof(buf));
 
-    while  (pcap)
+
+    while (pcap)
     {
-        uint8_t buf[1024];
-        memset(buf, 1, sizeof(buf));
+        struct pcap_pkthdr* pkthdr;
+        const u_char* data;
+        if (pcap_next_ex(pcap, &pkthdr, &data) != 1)
+            continue;
 
-        for (int i = 0; i < 5; i++)
+        plat_printf("成功接收数据\n");
+        int len = pkthdr->len > sizeof(buf) ? sizeof(buf) : pkthdr->len;
+        plat_memcpy(buf, data, len);
+
+        buf[0] = 1;
+        buf[1] = 2;
+
+        if (pcap_inject(pcap, buf, len) == -1)
         {
-            if (pcap_inject(pcap, buf, sizeof(buf)) == -1)
-            {
-                plat_printf("发送数据包失败: %s\n", pcap_geterr(pcap));
-                
-                return -1;
-            }
-            plat_printf("成功发送数据包\n");
+            plat_printf("发送数据失败: %s\n", pcap_geterr(pcap));
+            return -1;
         }
+        printf("发送数据成功\n");
     }
+        
+    
+
 
     return 0;
 }
